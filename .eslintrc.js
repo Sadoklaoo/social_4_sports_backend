f3b60b28259f5f86815de11a859ee11fe4972da0
Sadoklaoo <sadok.laouissi.sl@gmail.com>
2025-05-18 13:15:50 +0200

    feat: add ESLint configuration for TypeScript linting

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // your overrides…
+  },
+};
